bd8cf05d9f0d69e2d400b7621e72cf43b51a4fe2
ostev <82244060+ostev@users.noreply.github.com>
2025-09-28 15:22:58 +1000

    stuff

--- src/CombatantUI.gd
+++ src/CombatantUI.gd
@@ -1,7 +1,7 @@
 class_name CombatantUI extends Control
 
 @export var details: CombatantDetails
-@export var animated_sprite: AnimatedSprite2D
+@onready var animated_sprite: AnimatedSprite2D = $VBoxContainer/MarginContainer/AnimatedSprite2D
 var hp: int = 0
 
 @onready var health_bar: ProgressBar = $VBoxContainer/HealthBar
@@ -12,30 +12,31 @@ var encounter_manager: EncounterUIManager
 var index: int = 0
 
 func _ready() -> void:
-    pass
+	pass
 
 func set_details():
-    print(animated_sprite)
-    animated_sprite.sprite_frames = details.sprite_frames
-    _set_hp(details.max_hp)
-    health_bar.max_value = details.max_hp
-    label.text = details.name
+	print("sprite" + str(animated_sprite))
+	animated_sprite.sprite_frames = details.sprite_frames
+	animated_sprite.play("default")
+	_set_hp(details.max_hp)
+	health_bar.max_value = details.max_hp
+	label.text = details.name
 
 
 func die():
-    Globals.defeated_enemies.push_back(details.index)
-    encounter_manager.kill(index)
+	Globals.defeated_enemies.push_back(details.index)
+	encounter_manager.kill(index)
 
 func damage(amount: int) -> void:
-    _set_hp(hp - amount)
+	_set_hp(hp - amount)
 
-    if hp < 0:
-        die()
+	if hp <= 0:
+		die()
 
 func _set_hp(new_hp: int) -> void:
-    hp = new_hp
-    health_bar.value = new_hp
-    health_label.text = "%s/%s" % [new_hp, details.max_hp]
+	hp = new_hp
+	health_bar.value = new_hp
+	health_label.text = "%s/%s" % [new_hp, details.max_hp]
 
 func act(other: CombatantUI) -> void:
-    other.damage(5)
+	other.damage(5)
--- src/Enemy.gd
+++ src/Enemy.gd
@@ -26,3 +26,7 @@ func _trigger_battle(body: Node2D) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	_trigger_battle(body)
+
+
+func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	pass # Replace with function body.
